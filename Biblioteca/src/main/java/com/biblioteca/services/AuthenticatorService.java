f87799e2f1d614df58602f4066b475885b55ca3e
BIANCA ANTONELLY MEDEIROS DOS SANTOS <100948110+BiancaAntonelly@users.noreply.github.com>
2023-08-07 15:19:20 -0300

    Update AuthenticatorService.java

--- Biblioteca/src/main/java/com/biblioteca/services/AuthenticatorService.java
+++ Biblioteca/src/main/java/com/biblioteca/services/AuthenticatorService.java
@@ -12,7 +12,9 @@ import com.biblioteca.repositories.UsuarioRepository;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Optional;
 
@@ -85,116 +87,3 @@ public class AuthenticatorService {
 
 
 
-
-
-/*
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Service;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-@Service
-public class AuthenticatorService {
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-    private final String secret = "seu-segredo-aqui";
-
-    public String authenticate(String username, String password) {
-        // Implemente a lógica de autenticação aqui
-        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        if (passwordMatches(password, userDetails.getPassword())) {
-            return generateToken(userDetails);
-        }
-        return null; // Autenticação falhou
-    }
-
-
-    private boolean passwordMatches(String rawPassword, String encodedPassword) {
-        return passwordEncoder.matches(rawPassword, encodedPassword);
-    }
-
-
-    public void processJwtToken(HttpServletRequest request) {
-        final String authorizationHeader = request.getHeader("Authorization");
-
-        String username = null;
-        String jwt = null;
-
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            jwt = authorizationHeader.substring(7);
-            username = extractUsername(jwt);
-        }
-
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-
-            if (validateToken(jwt, userDetails)) {
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken
-                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-            }
-        }
-    }
-
-    private String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, userDetails.getUsername());
-    }
-
-    private String createToken(Map<String, Object> claims, String subject) {
-        Date now = new Date();
-        Date expirationDate = new Date(now.getTime() + 1000 * 60 * 60 * 10); // 10 horas
-
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(subject)
-                .setIssuedAt(now)
-                .setExpiration(expirationDate)
-                .compact();
-    }
-
-    private boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
-
-    private boolean isTokenExpired(String token) {
-        Date expiration = extractExpiration(token);
-        return expiration.before(new Date());
-    }
-
-    private String extractUsername(String token) {
-        return extractClaims(token).getSubject();
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaims(token).getExpiration();
-    }
-
-    private Claims extractClaims(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
-    }
-    
-    //comparar a senha
-}
-*/
