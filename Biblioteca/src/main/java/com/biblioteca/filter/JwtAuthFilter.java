28a1cd0010cd4cf10dde00bd3a8e87e18c4fe875
BIANCA ANTONELLY MEDEIROS DOS SANTOS <100948110+BiancaAntonelly@users.noreply.github.com>
2023-08-16 19:09:38 -0300

    atualização

--- Biblioteca/src/main/java/com/biblioteca/filter/JwtAuthFilter.java
+++ Biblioteca/src/main/java/com/biblioteca/filter/JwtAuthFilter.java
@@ -1,30 +1,40 @@
 package com.biblioteca.filter;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PublicKey;
 import java.util.ArrayList;
 
-public class JwtAuthFilter extends GenericFilterBean {
-	//GenericFilterBean é uma classe base fornecida pelo Spring para criar filtros
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private String jwtSigningKey = "minhachavesecretaminhachavesecretaminhachavesecretaminhachavesecretaminhachavesecreta";
+
+    //GenericFilterBean é uma classe base fornecida pelo Spring para criar filtros
 
     private final KeyPair keyPair = generateKeyPair();
     //a variável keyPair é inicializada com o resultado da chamada
 
     private KeyPair generateKeyPair() {
-    	//metodo privado só pode ser chamado dentro desta classe
+        //metodo privado só pode ser chamado dentro desta classe
         try {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
             //cria uma instância de KeyPairGrenerator usando a criptografia RSA
@@ -40,51 +50,30 @@ public class JwtAuthFilter extends GenericFilterBean {
     //usando o metodo generateKeyPair ele cria um par de chaves
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws IOException, ServletException {
-    	//o metodo doFilter é implementado na classe GenericFilterBean
-    	
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        //esta linha converte o objeto servletRequest em um objeto HttpServletRequest
-        //com o http você pode acessar os cabeçalhos
         String token = request.getHeader("Authorization");
-        //obtem o valor do cabeçalho Authorization
-        
         if (token != null && token.startsWith("Bearer ")) {
-        	//verifico se o token não é nulo e se tem o prefixo Bearer
             token = token.substring(7);
-            //se tiver remove ele  para ter só token
             try {
-                PublicKey publicKey = keyPair.getPublic();
-                //obtem o par de chaves, a chave será usada para validar a assinatura do token
-               
-                
-                JwtParser jwtParser = Jwts.parserBuilder().setSigningKey(publicKey).build();
-                //cria um analisador de token JWT usando a chave pública
-
+                JwtParser jwtParser = Jwts.parserBuilder()
+                        .setSigningKey(getSigningKey())
+                        .build();
                 Claims claims = jwtParser.parseClaimsJws(token).getBody();
-                // a função parseClaimsJwt verifica a assinatura do token
-                //getBody() extrai informações do token, nome de usuario, permissões
-       
+                String username = claims.getSubject();
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        claims.getSubject(), null, new ArrayList<>());
-                //cria uma instância de autenticação com o nome do usuário (assunto do token)
-                //claims.getSubject é o nome do usuario
-                //primeiro  null são as credenciais, como o token é uma prova de identidade
-                // a lista está vazia, vi que autorizações vazias da permissões completas
-                
+                        username, null, new ArrayList<>());
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-                // Define a autenticação no contexto de segurança do Spring, autentica o usuario associado ao token
-
             } catch (Exception e) {
-                // Token inválido
-                // Você pode tratar o erro aqui, como retornar uma resposta de erro 401
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                 return;
             }
         }
+        filterChain.doFilter(request, response);
+    }
 
-        filterChain.doFilter(servletRequest, servletResponse);
-        //concluiu o filtro
-        //recebe dois argumentos qaue são os ojbetos de solicitação e resposta dttp
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSigningKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
--- Biblioteca/src/main/java/com/biblioteca/services/AuthenticatorService.java
+++ Biblioteca/src/main/java/com/biblioteca/services/AuthenticatorService.java
@@ -10,6 +10,7 @@ import com.biblioteca.repositories.UsuarioRepository;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 
@@ -22,73 +23,47 @@ import java.util.Optional;
 @Service
 public class AuthenticatorService {
 
-    
-
     private UsuarioRepository usuarioRepository;
     
+    private String jwtSigningKey = "minhachavesecretaminhachavesecretaminhachavesecretaminhachavesecretaminhachavesecreta";
+
     @Autowired
     public AuthenticatorService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
 
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-    //Cria um objeto BCryptPasswordEncoder para codificar senhas
-    //É uma classe oferecida pelo spring security
-    //Essa clase implementa a interface passwordEncoder que verifica e codifica senhas seguras
-    
-    //private final String secret = "chave-secreta-token";
-    //assinatura do token
     
     public String authenticateUser(Auth auth) {
-    	System.out.println("Entrou no método authenticateUser");
-    	Optional<Usuario> storedUser = usuarioRepository.findByUsername(auth.getUsername());
-    	//buca no repositorio de usuarioRepository um usuario com o username
-        //caso exista um usuario ele é salvo em um optional
-    	if (storedUser.isPresent()) {
-    	//verifica se optional contém um usuario
+        System.out.println("Entrou no método authenticateUser");
+        Optional<Usuario> storedUser = usuarioRepository.findByUsername(auth.getUsername());
+
+        if (storedUser.isPresent()) {
             Usuario usuario = storedUser.get();
-            //se tiver um usuario no optional ele atribui esse usuario a variavel usuario
             boolean passwordMatches = passwordEncoder.matches(auth.getPassword(), usuario.getPassword());
-            //compara a senha do usuario com a senha do auth
+
             if (passwordMatches) {
-            	//se a senha for igual
-            	Key privateKey = keyPair.getPrivate();
-            	String token = generateToken(auth.getUsername(), privateKey);
-            	return "Autenticação bem sucedida no usuário: " + auth.getUsername() + "\nToken: " + token;
+                String token = generateToken(auth.getUsername());
+                return "Autenticação bem sucedida no usuário: " + auth.getUsername() + "\nToken: " + token;
             } else {
-            	return "Problema na autenticação do usuário: " + auth.getUsername();
+                return "Problema na autenticação do usuário: " + auth.getUsername();
             }
         } else {
-        	return "Usuário não encontrado";
+            return "Usuário não encontrado";
         }
     }
-    
 
     public String generateToken(String username) {
-    	//recebe o nome do usuario
-//    	   Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    	   //cria uma chave secreta key que é usada para assinar os tokens
         return Jwts.builder()
                 .setSubject(username)
-                //define o nome so usuario passado como argumento
                 .setIssuedAt(new Date())
-                //define o momento que o token foi criado
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 horas
-                //define o momento que o token vai expirar
-                .signWith(private Key, SignatureAlgorithm.RS256)
-                //indica que a chave do token será feita usando a chave key que foi criada
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
-        		//gera o token final e retorna 
-        
-
+    }
+    
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSigningKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
-
-
-
-
-
-
-
-
-
